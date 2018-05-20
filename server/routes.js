55f09b6973ea0ccfc488481533a1ec102d959ef6
WILL MEIER <willmeierart@gmail.com>
2018-05-19 18:16:18 -0600

    update rgba in gradient

--- server/routes.js
+++ server/routes.js
@@ -2,26 +2,26 @@
 export default {
   home: {
     bgImg: '/static/images/backgrounds/home_full.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   },
   live: {
     bgImg: '/static/images/backgrounds/live.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   },
   programs: {
     bgImg: '/static/images/backgrounds/programs.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   },
   cause: {
     bgImg: '/static/images/backgrounds/cause.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   },
   about: {
     bgImg: '/static/images/backgrounds/about.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   },
   contact: {
     bgImg: '/static/images/backgrounds/about.png',
-    bgColors: ['rgb(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
+    bgColors: ['rgba(233, 63, 58, ', 'rgba(240, 127, 62, ', 'rgba(249, 209, 71, ']
   }
 }
