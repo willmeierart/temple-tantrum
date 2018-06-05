d47c01cd0e2679d0ca1b8a87ba6e735f99200d38
WILL MEIER <willmeierart@gmail.com>
2018-06-04 20:19:57 -0600

    move style tag inside section of contact page

--- pages/contact.js
+++ pages/contact.js
@@ -10,40 +10,40 @@ const Contact = props => {
       <section>
         <div className='msg'>WE LOOK FORWARD TO HEARING FROM YOU!</div>
         <a href={`mailto:${email}`} target='_blank'><div className='email'>{ splitEmail }</div></a>
-      </section>
-      <style jsx>{`
-        section {
-          color: white;
-          font-family: leafy;
-          width: 100vw;
-          height: 110vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          position: relative;
-          letter-spacing: 0.125em;
-          font-size: 2em;
-          text-align: center;
-          z-index: 4;
-          margin-top: 10vh;
-        }
-        .email {
-          width: 100vw;
-          display: flex;
-          justify-content: center;
-          font-size: 2em;
-          flex-wrap: wrap;
-        }
-        .email:hover {
-          color: rgba(249, 209, 71);
-        }
-        @media screen and (max-width: 700px) {
-          .email {
-            font-size: .5em;
+        <style jsx>{`
+          section {
+            color: white;
+            font-family: leafy;
+            width: 100vw;
+            height: 110vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            letter-spacing: 0.125em;
+            font-size: 2em;
+            text-align: center;
+            z-index: 4;
+            margin-top: 10vh;
           }
-        }
-      `}</style>
+          .email {
+            width: 100vw;
+            display: flex;
+            justify-content: center;
+            font-size: 2em;
+            flex-wrap: wrap;
+          }
+          .email:hover {
+            color: rgba(249, 209, 71);
+          }
+          @media screen and (max-width: 700px) {
+            .email {
+              font-size: .5em;
+            }
+          }
+        `}</style>
+      </section>
     </AppProvider>
   )
 }
