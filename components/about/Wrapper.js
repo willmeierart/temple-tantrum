8bfd60e8764d0ba8b96d392848c26c3c43755c7f
WILL MEIER <willmeierart@gmail.com>
2018-08-08 20:35:49 -0600

    update about page styles

--- components/about/Wrapper.js
+++ components/about/Wrapper.js
@@ -8,7 +8,7 @@ class AboutWrapper extends Component {
     this.state = {
       isThin: false
     }
-    // binder(this, [''])
+    binder(this, ['renderTextModules'])
   }
 
   componentDidMount () {
@@ -27,81 +27,49 @@ class AboutWrapper extends Component {
     initWindow()
   }
 
+  renderTextModules () {
+    const aboutPage = this.props.data.allAboutPages ? this.props.data.allAboutPages : []
+    console.log('renderTM', aboutPage)
+    return aboutPage.map(sec => {
+      const { header, description } = sec
+      return (
+        <div className='wrapper'>
+          <div className='header-text'>{ header }</div>
+          <div className='body-text'>{ description }</div>
+          <style jsx>{`
+            .wrapper {
+              font-family: 'Verlag-Black';
+              width: 80%;
+              margin: auto;
+              color: white;
+              margin-bottom: 3em;
+              white-space: pre-line;
+            }
+            .header-text {
+              font-size: 3em;
+              margin-bottom: .5em;
+            }
+            .body-text {
+              font-size: 1.5em;
+            }
+          `}</style>
+        </div>
+      )
+    })
+  }
+
   render () {
-    const transformHeaderText = txt => {
-      if (txt) {
-        return txt.split(' ').map((wd, i) => (
-          <div key={i} className={i === txt.split(' ').length - 1 ? 'last' : ''}>
-            { wd }
-            <style jsx>{`
-              div {
-                text-transform: uppercase;
-                font-family: 'Leafy';
-                color: white;
-                text-align: center;
-                display: inline-block;
-              }
-              .last {
-                font-size: 2em;
-                display: block;
-              }
-            `}</style>
-          </div>
-        ))
-      }
-    }
-    const team = this.props.data.allTeams
-    const aboutPage = this.props.data.allAboutPages ? this.props.data.allAboutPages[0] : {}
     return (
       <div className='outer-wrapper'>
-        {this.props.data.allAboutPages && <div className='inner-wrapper'>
-          <div className='top-text'>
-            <div className='header-text'>{ transformHeaderText(aboutPage.header) }</div>
-            <div className='body'>{ aboutPage.description }</div>
-          </div>
-          {/* <div className='team-wrapper'>
-            <div className='header-text'>{ transformHeaderText(aboutPage.teamSectionHeader) }</div>
-            <TeamSection isThin={this.state.isThin} team={team} />
-          </div> */}
-        </div> }
+        { this.props.data.allAboutPages && <div className='inner-wrapper'>{ this.renderTextModules() }</div> }
         <style jsx>{`
           .outer-wrapper{
             margin-top: 20vh;
             margin-bottom: 150px;
-            position: relative;
             z-index: 4;
           }
           .inner-wrapper{
             padding-top: 20vh;
-            position: relative;
-          }
-          .top-text {
-            color: white;
-            font-family: 'Leafy';
-            display: flex;
-            justify-content: center;
-            position: relative;
-            text-align: center;
-          }
-          .team-wrapper {
-            position: relative;
-            margin-top: 10em;
-          }
-          .header-text {
-            position: absolute;
-            top: -5em;
-            left: 5%;
-            transform: rotate(-20deg);
-          }
-          .body {
-            column-count: 2;
-            width: 80%;
-            font-family: 'Verlag-Black';
-            text-align: left;
-            {/* padding: 2em; */}
-            column-gap: 2em;
-            font-size: 1.5em;
-            line-height: 1.25em;
           }
         `}</style>
       </div>
